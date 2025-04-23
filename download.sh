89bfa6a51e90db2c22cfd87649d6bfd66c2b0565
RH-Lin <51859630+RH-Lin@users.noreply.github.com>
2025-04-23 17:30:46 +0800

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,3 @@
+python download.py --output_dir ./videos --num_workers 16
+
+# if encounter ERROR: "[youtube] Sign in to confirm you’re not a bot. Use --cookies-from-browser or --cookies for the authentication. ", just wait for a couple hours to rerun it
